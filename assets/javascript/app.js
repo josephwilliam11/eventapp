ffb9feb58eeb87e6bbc0f169fe91a575e76558a4
joe arnold <joearnold@joes-MacBook-Air.local>
2018-04-10 22:03:55 -0600

    joe

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -130,3 +130,65 @@ var quill = new Quill("#editor-container", {
   placeholder: "Compose an email here...",
   theme: "snow" // or 'bubble'
 });
+
+//-------------------------------------------------
+//login
+//-------------------------------------------------
+$(".form")
+  .find("input, textarea")
+  .on("keyup blur focus", function(e) {
+    var $this = $(this),
+      label = $this.prev("label");
+
+    if (e.type === "keyup") {
+      if ($this.val() === "") {
+        label.removeClass("active highlight");
+      } else {
+        label.addClass("active highlight");
+      }
+    } else if (e.type === "blur") {
+      if ($this.val() === "") {
+        label.removeClass("active highlight");
+      } else {
+        label.removeClass("highlight");
+      }
+    } else if (e.type === "focus") {
+      if ($this.val() === "") {
+        label.removeClass("highlight");
+      } else if ($this.val() !== "") {
+        label.addClass("highlight");
+      }
+    }
+  });
+
+$(".tab a").on("click", function(e) {
+  e.preventDefault();
+
+  $(this)
+    .parent()
+    .addClass("active");
+  $(this)
+    .parent()
+    .siblings()
+    .removeClass("active");
+
+  target = $(this).attr("href");
+
+  $(".tab-content > div")
+    .not(target)
+    .hide();
+
+  $(target).fadeIn(600);
+});
+
+function toggle() {
+  if (document.getElementById("hidethis").style.display == "none") {
+    document.getElementById("hidethis").style.display = "table-row"; // set to table-row instead of an empty string
+  } else {
+    document.getElementById("hidethis").style.display = "none";
+  }
+}
+
+function checkZip(value) {
+  return /(^\d{5}$)|(^\d{5}-\d{4}$)/.test(value);
+}
